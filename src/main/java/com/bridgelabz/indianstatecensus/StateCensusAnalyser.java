83fec9d3d5452bbb8b0a472b573dbf40d6e909ec
prem-12345 <premkpatil1997@gmail.com>
2021-09-24 17:37:07 +0530

    Added load states census csv data method

--- src/main/java/com/bridgelabz/indianstatecensus/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/indianstatecensus/StateCensusAnalyser.java
@@ -1,8 +1,48 @@
 package com.bridgelabz.indianstatecensus;
 
-public class StateCensusAnalyser {
-    public static void main(String[] args) {
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
-        // "WELCOME TO INDIAN STATES CENSUS ANALYSER PROBLEM"
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+
+public class StateCensusAnalyser {
+    String filePath = "IndianStateCensus.csv";
+
+    public static void main(String[] args) throws IOException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        stateCensusAnalyser.loadStateCensusCsvData();
+        stateCensusAnalyser.numberOfRecords();
+    }
+
+    public void loadStateCensusCsvData() {
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            CSVReader csvReader = new CSVReaderBuilder(fileReader)
+                    .build();
+            List<String[]> allData = csvReader.readAll();
+            for (String[] row : allData) {
+                for (String cell : row) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int numberOfRecords() throws IOException {
+        FileReader fileReader = new FileReader(filePath);
+        CSVReader csvReader = new CSVReaderBuilder(fileReader)
+                .withSkipLines(1)
+                .build();
+        List<String[]> allData = csvReader.readAll();
+        int numberOfRecords;
+        numberOfRecords = allData.size();
+        System.out.println("Total Number Of Records: "+numberOfRecords);
+        return numberOfRecords;
     }
 }
