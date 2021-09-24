499b53c13381daaf239ba4da3bf3329ecd8fbe7c
prem-12345 <premkpatil1997@gmail.com>
2021-09-24 18:09:55 +0530

    Added test case for wrong csv file path

--- src/test/java/com/bridgelabz/indianstatecensus/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/indianstatecensus/StateCensusAnalyserTest.java
@@ -1,13 +1,13 @@
 package com.bridgelabz.indianstatecensus;
 
 import org.junit.jupiter.api.Test;
-
+import org.junit.rules.ExpectedException;
 import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class StateCensusAnalyserTest {
     String filePath = "IndianStateCensus.csv";
+    String wrongCsvFilePath = "IndianStateCensusWrongPath.csv";
 
     @Test
     public void givenIndianStateCensusCsvFile_shouldReturn_correctNoOfRecords() {
@@ -20,4 +20,15 @@ class StateCensusAnalyserTest {
         }
     }
 
+    @Test
+    public void givenIndianStateCensusWrongCsvFilePath_shouldThrowException() throws IOException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(StateCensusAnalyserException.class);
+            stateCensusAnalyser.numberOfRecords(wrongCsvFilePath);
+        } catch (StateCensusAnalyserException e) {
+            assertEquals(StateCensusAnalyserException.ExceptionType.WRONG_CSV_FILE_PATH, e.type);
+        }
+    }
 }
